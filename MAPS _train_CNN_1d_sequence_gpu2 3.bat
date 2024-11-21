673cf722eeeea7d7d2e3252c4119942f71974934
Ming Wu <mingwucn@outlook.com>
2024-11-21 21:25:13 +0100

    CNN_1d_cross_validation_with_differnt_folds_for_context_classification_1_4 From barabas

--- MAPS _train_CNN_1d_sequence_gpu2 3.bat
+++ MAPS _train_CNN_1d_sequence_gpu2 3.bat
@@ -5,14 +5,20 @@ set "TOPIC=CNN_1d_cross_validation_with_differnt_folds_for_context_classificatio
 set "WORKDIR=$WORKDIR/MuSIC/Mitigate"
 set "server=barabas"
 set "batch_size=1024"
+set "epochs=50"
+set "folds=10"
+set "repeat=1"
 
-set "ARG1=--gpu=2 --save_every=10 --fold_i=0 --folds=10 --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=1 --epochs=100 --num_workers=12 --input_type='mic' --output_type='direction'"
-set "ARG2=--gpu=2 --save_every=10 --fold_i=0 --folds=10 --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=1 --epochs=100 --num_workers=12 --input_type='mic+energy' --output_type='position'"
+set "ARG1=--gpu=2 --save_every=10 --fold_i=0 --folds=folds --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=repeat --epochs=epochs --num_workers=12 --input_type='mic' --output_type='direction'"
+set "ARG2=--gpu=2 --save_every=10 --fold_i=0 --folds=folds --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=repeat --epochs=epochs --num_workers=12 --input_type='mic+energy' --output_type='direction'"
+set "ARG3=--gpu=2 --save_every=10 --fold_i=0 --folds=folds --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=repeat --epochs=epochs --num_workers=12 --input_type='mic' --output_type='position'"
+set "ARG4=--gpu=2 --save_every=10 --fold_i=0 --folds=folds --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=repeat --epochs=epochs --num_workers=12 --input_type='mic+energy' --output_type='position'"
+set "ARG5=--gpu=3 --save_every=10 --fold_i=0 --folds=folds --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=repeat --epochs=epochs --num_workers=12 --input_type='ae' --output_type='direction'"
+set "ARG6=--gpu=3 --save_every=10 --fold_i=0 --folds=folds --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=repeat --epochs=epochs --num_workers=12 --input_type='ae+energy' --output_type='direction'"
+set "ARG7=--gpu=3 --save_every=10 --fold_i=0 --folds=folds --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=repeat --epochs=epochs --num_workers=12 --input_type='ae' --output_type='position'"
+set "ARG8=--gpu=3 --save_every=10 --fold_i=0 --folds=folds --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=repeat --epochs=epochs --num_workers=12 --input_type='ae+energy' --output_type='position'"
 
-set "ARG3=--gpu=3 --save_every=10 --fold_i=0 --folds=10 --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=1 --epochs=100 --num_workers=12 --input_type='ae' --output_type='direction'"
-set "ARG4=--gpu=3 --save_every=10 --fold_i=0 --folds=10 --model_name=CNN --batch_size=!batch_size! --learning_rate=5e-4 --repeat=1 --epochs=100 --num_workers=12 --input_type='ae+energy' --output_type='position'"
-
-set "TOPICI=!TOPIC!_1_2"
+set "TOPICI=!TOPIC!_1_4"
 set "cmd="
 for /l %%i in (1,1,2) do (
     set "cmd=!cmd! torchrun train_dist_1d.py !ARG%%i! &&"
@@ -29,7 +35,7 @@ ssh -t !server! "tmux new -s !TOPICI! -n 0 -d"
 ssh !server! "tmux send -t !TOPICI!:0 'cd !WORKDIR! && !GIT!' C-m"
 ssh !server! "tmux send -t !TOPICI!:0 'clear && cd !WORKDIR! && !cmd! && cd ./lfs && !GIT!' C-m"
 
-set "TOPICI=!TOPIC!_3_4"
+set "TOPICI=!TOPIC!_5_8"
 set "cmd="
 for /l %%i in (3,1,4) do (
     set "cmd=!cmd! python train_dist_1d.py !ARG%%i! &&"
